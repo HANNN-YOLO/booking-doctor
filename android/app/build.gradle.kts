65d4758f0e2496d9765a840b891e5aec41a6288b
Khai1205 <muh.imamannafri12@gmail.com>
2025-05-08 10:28:06 +0800

    firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
@@ -42,3 +44,9 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation 'com.google.firebase:firebase-analytics' // Contoh untuk Firebase Analytics
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+}
+
