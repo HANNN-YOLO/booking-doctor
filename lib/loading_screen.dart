a2663f1fa278002a7b0bfeec4429fc2d29ab9368
HANNN-YOLO <reyhanrafaidhil1126@gmail.com>
2025-06-17 20:29:26 +0700

    test hosting

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -16,6 +16,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Future<void> _navigateToNext() async {
     await Future.delayed(Duration(seconds: 3)); // durasi loading
     Navigator.pushReplacementNamed(context, '/login'); // ganti sesuai kebutuhan
+    // context.go('/login');
+    // Jika ingin mengganti halaman sekarang
   }
 
   @override
@@ -41,3 +43,53 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+
+// class LoadingScreen extends StatefulWidget {
+//   static const String route = '/loading';
+
+//   @override
+//   _LoadingScreenState createState() => _LoadingScreenState();
+// }
+
+// class _LoadingScreenState extends State<LoadingScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     // Navigasi setelah frame dibangun (agar context valid)
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       _navigateToNext();
+//     });
+//   }
+
+//   Future<void> _navigateToNext() async {
+//     await Future.delayed(Duration(seconds: 3)); // durasi splash
+//     context.go('/login'); // arahkan ke halaman login
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0xFF81C784), // hijau muda
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               'assets/logo-removebg-preview.png',
+//               width: 150,
+//               height: 150,
+//             ),
+//             const SizedBox(height: 20),
+//             const CircularProgressIndicator(
+//               color: Colors.white,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
